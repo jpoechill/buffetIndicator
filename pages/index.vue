3cffce6a4149d27352e3e38eeef47793a1bea193
Po Rith <poamrong@rith.com>
2020-08-14 13:40:15 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <div>
+      Ooga Booga <br><br>
+      <img src="https://s3.amazonaws.com/gurufocus_charts/1597435437161.png">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
